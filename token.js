43706bf860c487e4ac520b11f231c68e6595c0fd
Mora Hosting <bimamozuofficial@gmail.com>
2026-02-01 01:21:53 +0700

    create token

---
+++ token.js
@@ -0,0 +1,30 @@
+{
+  "owners": [
+    "6324900045"
+  ],
+  "config": {
+    "channel_id": "@parsoksa16",
+    "group_id": "-1003247411758",
+    "group_link": "https://t.me/wardigmora"
+  },
+  "tokens": [
+    "7780421509:AAFzBYn7JRMrF67BgMskH1lo3U-fZNMKhkE",
+    {
+      "key": "REZZ-C058D9",
+      "expired": 1768834080649,
+      "created_by": 6324900045
+    },
+    "8545131465:AAFVqP_bmCSeHbh4WSqmZaEeT8YX3HyXgrc",
+    "gw",
+    "8308126130:AAFMNp9iIrmwF3ELwAdaXcsnj66kVL2Gv90",
+  ],
+  "sha": "6e9d0289da694067220709f4860fbcdc5e82cb1a",
+  "list_buyer": [
+    {
+      "id": "6324900045",
+      "name": "⏤‌‌‌‌ 𝗧‌𝗿𝗶𝗽‌𝗹𝗲 𝗫 || 𝙈𝙊𝙍𝘼 𝗢‌𝗳𝗳𝗶‌𝗰𝗶‌𝗮𝗹 c٥ʷₚ‌៰ᷜ⍳٥ˢ",
+      "username": "moraxjun",
+      "role": "ᴏᴡɴᴇƦ"
+    },
+  ]
+      }
